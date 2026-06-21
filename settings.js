79bca147e81bcb0966046192b6e30d34d08ca45c
paxbarofficial <paxbarcommunity@gmail.com>
2026-06-21 11:52:32 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ global.version = "ᴠ𝟹.𝟶"
 global.session = "sessions"
 module.exports = { 
     NAMA_OWNER: "*ʙᴀʀᴍᴏᴅꜱꜱ ᴏꜰꜰɪᴄɪᴀʟ*",
-    NAMA_BOT: "PAXBAR V2",
+    NAMA_BOT: "PAXBAR V3",
     NUMBER_BOT: "6283830646778",
-    WM: "SCRIPT PAXBAR V2",
-    PACNAME: "🦄 PAXBAR VERSION 2",
+    WM: "SCRIPT PAXBAR V3",
+    PACNAME: "🦄 PAXBAR VERSION 3",
     TELEGRAM: "anonympaxx.t.me",
 };
